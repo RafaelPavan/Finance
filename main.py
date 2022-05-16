211f13b2aa82aa00fab9d4d4ee1059fd1855dda5
RafaelPavan <90516421+RafaelPavan@users.noreply.github.com>
2022-05-16 17:38:07 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from filtra_dados.scraper import valor_dolar, valor_euro, valor_libra, valor_iene
 from interfaces import interface
+from querys.user_query import QueryCustomer
 
 if __name__ == '__main__':
     # print(valor_dolar.text)
@@ -8,6 +9,11 @@ if __name__ == '__main__':
 
 
 class Cots:
+    @staticmethod
+    def show_quotations():
+        interface.cotacoes.show()
+        interface.login.close()
+
     @staticmethod
     def dolar_cot():
         interface.dolar.show()
@@ -52,5 +58,29 @@ class Cots:
         interface.iene.close()
         interface.cotacoes.show()
 
-class Cadastra:
-    pass
+
+class Register(object):
+
+    @staticmethod
+    def aparece_usuario():
+        interface.register.show()
+        interface.login.close()
+
+    @staticmethod
+    def register_user():
+        name = interface.register.lineEdit.text()
+        surname = interface.register.lineEdit_2.text()
+        cpf = interface.register.lineEdit_3.text()
+        user_login = interface.register.lineEdit_4.text()
+        password = interface.register.lineEdit_5.text()
+
+        try:
+            QueryCustomer().insert_user(name, surname, cpf, user_login, password)
+            interface.register.label_2.setText("Cliente cadastrado com sucesso!")
+        except Exception as erro:
+            print(erro)
+
+    @staticmethod
+    def back_login():
+        interface.login.show()
+        interface.register.close()
